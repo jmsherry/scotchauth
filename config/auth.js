da1e6c9ffd67cdfa5fa475bb85f82614d4773ed8
James Sherry <james.m.sherry@googlemail.com>
2019-04-22 04:41:37 +0100

    changed callback routes to target heroku domain

--- config/auth.js
+++ config/auth.js
@@ -13,7 +13,7 @@ module.exports = {
     facebookAuth : {
         'clientID'        : FACEBOOK_CLIENT_ID, //'1832132633772519', // your App ID
         'clientSecret'    : FACEBOOK_CLIENT_SECRET, // your App Secret
-        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
+        'callbackURL'     : 'https://oauthdemo225.herokuapp.com/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
 
     },
@@ -21,12 +21,12 @@ module.exports = {
     twitterAuth : {
         'consumerKey'        : TWITTER_CONSUMER_KEY,
         'consumerSecret'     : TWITTER_CONSUMER_SECRET,
-        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL'        : 'https://oauthdemo225.herokuapp.com/auth/twitter/callback'
     },
 
     googleAuth : {
         'clientID'         : GOOGLE_CLIENT_ID,
         'clientSecret'     : GOOGLE_CLIENT_SECRET,
-        'callbackURL'      : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'      : 'https://oauthdemo225.herokuapp.com/auth/google/callback'
     }
 };
