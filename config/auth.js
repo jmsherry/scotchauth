639cb35b25c7838732b8a896aad89c9ed0d2ba72
James Sherry <james.m.sherry@googlemail.com>
2019-04-22 03:33:42 +0100

    initial files

---
+++ config/auth.js
@@ -0,0 +1,32 @@
+// expose our config directly to our application using module.exports
+const {
+  FACEBOOK_CLIENT_ID,
+  FACEBOOK_CLIENT_SECRET,
+  TWITTER_CONSUMER_KEY,
+  TWITTER_CONSUMER_SECRET
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+} = process.env;
+
+module.exports = {
+
+    facebookAuth : {
+        'clientID'        : FACEBOOK_CLIENT_ID //'1832132633772519', // your App ID
+        'clientSecret'    : FACEBOOK_CLIENT_SECRET, // your App Secret
+        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
+        'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
+
+    },
+
+    twitterAuth : {
+        'consumerKey'        : TWITTER_CONSUMER_KEY,
+        'consumerSecret'     : TWITTER_CONSUMER_SECRET,
+        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    googleAuth : {
+        'clientID'         : GOOGLE_CLIENT_ID,
+        'clientSecret'     : GOOGLE_CLIENT_SECRET,
+        'callbackURL'      : 'http://localhost:8080/auth/google/callback'
+    }
+};
---
+++ server.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+
+// set up ======================================================================
+// get all the tools we need
+const express  = require('express');
+const app      = express();
+const mongoose = require('mongoose');
+const redis = require('redis');
+const client  = redis.createClient();
+const passport = require('passport');
+const flash    = require('connect-flash');
+const exphbs = require('express-handlebars');
+const morgan       = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser   = require('body-parser');
+const session      = require('express-session');
+const redisStore = require('connect-redis')(session);
+
+const {
+  MONGODB_URI,
+  PORT,
+  REDIS_URL
+} = process.env;
+
+// configuration ===============================================================
+mongoose.promise = global.Promise
+const promise = mongoose.connect(MONGODB_URI, { useNewUrlParser: true }); // connect to our database
+promise.then(function(db) {
+    console.log('DATABASE CONNECTED!!');
+}).catch(function(err){
+    console.log('CONNECTION ERROR', err);
+});
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+app.engine('.hbs', exphbs());
+app.set('views', __dirname + '/views')
+app.set('view engine', '.hbs');
+
+// required for passport
+app.use(session({
+  secret: 'ilovescotchscotchyscotchscotch',
+  store: new redisStore({ host: REDIS_URL, port: 6379, client,
+  ttl :  260}),
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(PORT, function(){
+  console.log('The magic happens on port ' + PORT);
+});
+
+module.exports = app;
